6651fecff69f3a8fde54cef36bfe2dfe4aa8e965
samcarlos <samcarlos@gmail.com>
2015-05-26 10:47:08 -0500

    removed "lagged_theta"

--- HSSD_Test_1.R
+++ HSSD_Test_1.R
@@ -28,7 +28,6 @@ parameters {
 }
 transformed parameters {
   matrix[N,T] theta;
-  matrix[N,T] lagged_theta;
   matrix[N,T] explanvars;  
 
 
@@ -42,20 +41,21 @@ transformed parameters {
           explanvars[n,t]<-beta[n,p +(l-1)*P]*x_data[t-(l-1),p,n] + explanvars[n,t];
 
 
-  for(n in 1:N)
-    for(t in (1+L):T)
-      for(l in 1:(L-1))
-        lagged_theta[n,t]<-beta[n,P*L+l]*theta[n,t-l] + lagged_theta[n,t];
-
 
   for(n in 1:N) for(t in 1:(L+1)) theta[n,t]<-y_data[t,n];
 
 
   for (n in 1:N)
     for (t in (2+L):T){
-      theta[n,t]<-lagged_theta[n,t] + explanvars[n,t] +sigmasq_theta[n]*theta_innovation[t,n];
+      theta[n,t]<- explanvars[n,t] +sigmasq_theta[n]*theta_innovation[t,n];
     }
+  
 
+  for (n in 1:N)
+    for (t in (2+L):T)
+      for(l in 1:(L-1))
+        theta[n,t]<-beta[n,P*L+l]*theta[n,t-l]+theta[n,t];
+    
 }
 
 model {
@@ -80,4 +80,5 @@ model {
 
 
 "
-fit <- stan(model_code = model, data = stan.dat, iter = 100, chains = 1, verbose = TRUE)
+fit <- stan(model_code = model, data = stan.dat, iter = 1000, chains = 4, verbose = TRUE)
+mat=as.matrix(fit)
